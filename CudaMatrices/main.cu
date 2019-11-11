ee5d73df5e29428cac66ecf7ab5f6101bcae17c5
austi <austin.green@outlook.com>
2019-11-10 21:31:42 -0800

    Add tests and performance output

--- CudaMatrices/main.cu
+++ CudaMatrices/main.cu
@@ -4,13 +4,13 @@
 #include <device_functions.h>
 #include <cuda_runtime_api.h>
 #include <cublas_v2.h>
-
+#include <ctime>
 #include <algorithm>
 #include <stdio.h>
 
-constexpr auto TILE_SIZE = 2;
+constexpr auto TILE_SIZE = 16;
 
-__global__ void NativeMMKernel(float *a, float *b, float *c, int size)
+__global__ void NaiveMMKernel(float *a, float *b, float *c, int size)
 {
 	int xOut = blockDim.x * blockIdx.x + threadIdx.x;
 	int yOut = blockDim.y * blockIdx.y + threadIdx.y;
@@ -36,6 +36,9 @@ __global__ void OptimizedMMKernel(float *a, float *b, float *c, int size)
 	// Each block loads a shared block from A and B so the partial sums can
 	// be done at the same time
 
+	int tx = threadIdx.x;
+	int ty = threadIdx.y;
+
 	int x = blockIdx.x * blockDim.x + threadIdx.x;
 	int y = blockIdx.y * blockDim.y + threadIdx.y;
 
@@ -44,15 +47,15 @@ __global__ void OptimizedMMKernel(float *a, float *b, float *c, int size)
 	for (int i = 0; i < tilesPerGrid; i++)
 	{
 		// Each thread loads element into A and B
-		sharedA[threadIdx.y * TILE_SIZE + threadIdx.x] = a[(y * size) + (i * TILE_SIZE) + threadIdx.x];
-		sharedB[threadIdx.y * TILE_SIZE + threadIdx.x] = b[(i * TILE_SIZE * size) + (threadIdx.y * size) + x];
+		sharedA[ty * TILE_SIZE + tx] = a[(y * size) + (i * TILE_SIZE) + tx];
+		sharedB[ty * TILE_SIZE + tx] = b[(i * TILE_SIZE * size) + (ty * size) + x];
 
 		// Wait for all threads to load each section of the shared matrix
 		__syncthreads();
 
 		for (int j = 0; j < TILE_SIZE; j++)
 		{
-			sum += sharedA[threadIdx.y * TILE_SIZE + j] * sharedB[j * TILE_SIZE + threadIdx.x];
+			sum = sum + sharedA[ty * TILE_SIZE + j] * sharedB[j * TILE_SIZE + tx];
 		}
 
 		// Wait for all threads to compute their partial sum from the shared matrices before loading the next
@@ -95,167 +98,172 @@ void CheckCudaCall(cudaError_t callResult, const char *message)
 	}
 }
 
-float* NativeMM(float *a, float *b, int size, bool timeMemory, float *elapsed)
+float* NaiveMM(float *a, float *b, int size, float *kernelTime, float* totalTime)
 {
 	const int matrixSizeBytes = size * size * sizeof(float);
 	float *result = new float[size * size];
 	float *devA, *devB, *devC;
 
 	// Create events
-	cudaEvent_t startEvent, stopEvent;
-	CheckCudaCall(cudaEventCreate(&startEvent), "cudaEventCreate failed");
-	CheckCudaCall(cudaEventCreate(&stopEvent), "cudaEventCreate failed");
+	cudaEvent_t kernelStart, kernelStop, totalStart, totalStop;
+	CheckCudaCall(cudaEventCreate(&kernelStart), "cudaEventCreate failed");
+	CheckCudaCall(cudaEventCreate(&kernelStop), "cudaEventCreate failed");
+	CheckCudaCall(cudaEventCreate(&totalStart), "cudaEventCreate failed");
+	CheckCudaCall(cudaEventCreate(&totalStop), "cudaEventCreate failed");
 
 	// Allocate device matrices
  	CheckCudaCall(cudaMalloc((void **)&devA, matrixSizeBytes), "cudaMalloc failed");
 	CheckCudaCall(cudaMalloc((void **)&devB, matrixSizeBytes), "cudaMalloc failed");
 	CheckCudaCall(cudaMalloc((void **)&devC, matrixSizeBytes), "cudaMalloc failed");
 
-	// If we're timing memory communication, start the timer here
-	if (timeMemory)
-	{
-		cudaEventRecord(startEvent, 0);
-	}
+	// Sstart the timer here for the total time
+	cudaEventRecord(totalStart, 0);
 
 	// Copy over to host
 	CheckCudaCall(cudaMemcpy(devA, a, matrixSizeBytes, cudaMemcpyHostToDevice), "cudaMemcpy failed");
 	CheckCudaCall(cudaMemcpy(devB, b, matrixSizeBytes, cudaMemcpyHostToDevice), "cudaMemcpy failed");
 
-	// If we're NOT timing memory communication, start the timer here
-	if (!timeMemory)
-	{
-		cudaEventRecord(startEvent, 0);
-	}
+	// Start the timer here for the kernel time
+	cudaEventRecord(kernelStart, 0);
 
 	// Call kernel
-	dim3 blockSize(size / TILE_SIZE, size / TILE_SIZE);
-	dim3 threadsPerBlock(TILE_SIZE, TILE_SIZE);	
-	NativeMMKernel<<<blockSize, threadsPerBlock>>>(devA, devB, devC, size);
+	const int tpb = 16;
+	dim3 blockSize(size / tpb, size / tpb);
+	dim3 threadsPerBlock(tpb, tpb);
+	NaiveMMKernel<<<blockSize, threadsPerBlock>>>(devA, devB, devC, size);
 
-	// If we're NOT timing memory communication, stop the timer here
-	if (!timeMemory)
-	{
-		cudaEventRecord(stopEvent, 0);
-		cudaEventSynchronize(stopEvent);
-	}
+	// Stop kernel timer here
+	cudaEventRecord(kernelStop, 0);
+	cudaEventSynchronize(kernelStop);
 
 	// Copy result back
 	cudaMemcpy(result, devC, matrixSizeBytes, cudaMemcpyDeviceToHost);
 
-	// If we're timing memory communication, stop the timer here
-	if (timeMemory)
-	{
-		cudaEventRecord(stopEvent, 0);
-		cudaEventSynchronize(stopEvent);
-	}
+	// Stop total time here
+	cudaEventRecord(totalStop, 0);
+	cudaEventSynchronize(totalStop);
 
-	// Calculate elapsed time
-	cudaEventElapsedTime(elapsed, startEvent, stopEvent);
+	// Calculate elapsed times
+	cudaEventElapsedTime(kernelTime, kernelStart, kernelStop);
+	cudaEventElapsedTime(totalTime, totalStart, totalStop);
 
 	// Cleanup device memory
 	cudaFree(devA);
 	cudaFree(devB);
 	cudaFree(devC);
-
-	// Cleanup host memory
-	delete[] a;
-	delete[] b;
 	
+	// Return result
 	return result;
 }
 
-float* OptimizedMM(float *a, float *b, int size, bool timeMemory, float *elapsed)
+float* OptimizedMM(float *a, float *b, int size, float *kernelTime, float* totalTime)
 {
 	const int matrixSizeBytes = size * size * sizeof(float);
 	float *result = new float[size * size];
 	float *devA, *devB, *devC;
 
 	// Create events
-	cudaEvent_t startEvent, stopEvent;
-	CheckCudaCall(cudaEventCreate(&startEvent), "cudaEventCreate failed");
-	CheckCudaCall(cudaEventCreate(&stopEvent), "cudaEventCreate failed");
+	cudaEvent_t kernelStart, kernelStop, totalStart, totalStop;
+	CheckCudaCall(cudaEventCreate(&kernelStart), "cudaEventCreate failed");
+	CheckCudaCall(cudaEventCreate(&kernelStop), "cudaEventCreate failed");
+	CheckCudaCall(cudaEventCreate(&totalStart), "cudaEventCreate failed");
+	CheckCudaCall(cudaEventCreate(&totalStop), "cudaEventCreate failed");
 
 	// Allocate device matrices
 	CheckCudaCall(cudaMalloc((void **)&devA, matrixSizeBytes), "cudaMalloc failed");
 	CheckCudaCall(cudaMalloc((void **)&devB, matrixSizeBytes), "cudaMalloc failed");
 	CheckCudaCall(cudaMalloc((void **)&devC, matrixSizeBytes), "cudaMalloc failed");
 
-	// If we're timing memory communication, start the timer here
-	if (timeMemory)
-	{
-		cudaEventRecord(startEvent, 0);
-	}
+	// Sstart the timer here for the total time
+	cudaEventRecord(totalStart, 0);
 
 	// Copy over to host
 	CheckCudaCall(cudaMemcpy(devA, a, matrixSizeBytes, cudaMemcpyHostToDevice), "cudaMemcpy failed");
 	CheckCudaCall(cudaMemcpy(devB, b, matrixSizeBytes, cudaMemcpyHostToDevice), "cudaMemcpy failed");
 
-	// If we're NOT timing memory communication, start the timer here
-	if (!timeMemory)
-	{
-		cudaEventRecord(startEvent, 0);
-	}
+	// Start the timer here for the kernel time
+	cudaEventRecord(kernelStart, 0);
 
 	// Call kernel
 	dim3 blockSize(size / TILE_SIZE, size / TILE_SIZE);
 	dim3 threadsPerBlock(TILE_SIZE, TILE_SIZE);
+	OptimizedMMKernel <<<blockSize, threadsPerBlock >>>(devA, devB, devC, size);
 
-	// TODO: Pass in Shared memory size (http://courses.cms.caltech.edu/cs179/2019_lectures/cs179_2019_lec10.pdf)
-	OptimizedMMKernel<<<blockSize, threadsPerBlock>>>(devA, devB, devC, size);
-
-	// If we're NOT timing memory communication, stop the timer here
-	if (!timeMemory)
-	{
-		cudaEventRecord(stopEvent, 0);
-		cudaEventSynchronize(stopEvent);
-	}
+	// Stop kernel timer here
+	cudaEventRecord(kernelStop, 0);
+	cudaEventSynchronize(kernelStop);
 
 	// Copy result back
 	cudaMemcpy(result, devC, matrixSizeBytes, cudaMemcpyDeviceToHost);
 
-	// If we're timing memory communication, stop the timer here
-	if (timeMemory)
-	{
-		cudaEventRecord(stopEvent, 0);
-		cudaEventSynchronize(stopEvent);
-	}
+	// Stop total time here
+	cudaEventRecord(totalStop, 0);
+	cudaEventSynchronize(totalStop);
 
-	// Calculate elapsed time
-	cudaEventElapsedTime(elapsed, startEvent, stopEvent);
+	// Calculate elapsed times
+	cudaEventElapsedTime(kernelTime, kernelStart, kernelStop);
+	cudaEventElapsedTime(totalTime, totalStart, totalStop);
 
 	// Cleanup device memory
 	cudaFree(devA);
 	cudaFree(devB);
 	cudaFree(devC);
 
+	// Return result
 	return result;
 }
 
-float* CublasMM(cublasHandle_t &handle, float *a, float *b, int size)
+float* CublasMM(cublasHandle_t &handle, float *a, float *b, int size, float *kernelTime, float* totalTime)
 {
 	const int matrixSizeBytes = size * size * sizeof(float);
 	float *result = new float[size * size];
 	float *devA, *devB, *devC;
+
+	// Create events
+	cudaEvent_t kernelStart, kernelStop, totalStart, totalStop;
+	CheckCudaCall(cudaEventCreate(&kernelStart), "cudaEventCreate failed");
+	CheckCudaCall(cudaEventCreate(&kernelStop), "cudaEventCreate failed");
+	CheckCudaCall(cudaEventCreate(&totalStart), "cudaEventCreate failed");
+	CheckCudaCall(cudaEventCreate(&totalStop), "cudaEventCreate failed");
 
 	// Allocate device matrices
 	CheckCudaCall(cudaMalloc((void **)&devA, matrixSizeBytes), "cudaMalloc failed");
 	CheckCudaCall(cudaMalloc((void **)&devB, matrixSizeBytes), "cudaMalloc failed");
 	CheckCudaCall(cudaMalloc((void **)&devC, matrixSizeBytes), "cudaMalloc failed");
 
+	// Sstart the timer here for the total time
+	cudaEventRecord(totalStart, 0);
+
 	// Copy over to host
 	CheckCudaCall(cudaMemcpy(devA, a, matrixSizeBytes, cudaMemcpyHostToDevice), "cudaMemcpy failed");
 	CheckCudaCall(cudaMemcpy(devB, b, matrixSizeBytes, cudaMemcpyHostToDevice), "cudaMemcpy failed");
 
+	// Start the timer here for the kernel time
+	cudaEventRecord(kernelStart, 0);
+
+	// Initialize cublas params
 	const float alp = 1;
 	const float bet = 0;
 	const float *alpha = &alp;
 	const float *beta = &bet;
 
 	// Do the actual multiplication
-	cublasSgemm(handle, CUBLAS_OP_N, CUBLAS_OP_N, size, size, size, alpha, devA, size, devB, size, beta, devC, size);
+	cublasSgemm(handle, CUBLAS_OP_T, CUBLAS_OP_T, size, size, size, alpha, devA, size, devB, size, beta, devC, size);
+
+	// Stop kernel timer here
+	cudaEventRecord(kernelStop, 0);
+	cudaEventSynchronize(kernelStop);
 
 	// Copy result back
 	CheckCudaCall(cudaMemcpy(result, devC, matrixSizeBytes, cudaMemcpyDeviceToHost), "cudaMemcpy failed");
+
+	// Stop total time here
+	cudaEventRecord(totalStop, 0);
+	cudaEventSynchronize(totalStop);
+
+	// Calculate elapsed times
+	cudaEventElapsedTime(kernelTime, kernelStart, kernelStop);
+	cudaEventElapsedTime(totalTime, totalStart, totalStop);
 
 	// Cleanup device memory
 	cudaFree(devA);
@@ -270,7 +278,8 @@ void RandomizeMatrix(float *mat, int size)
 {
 	for (int i = 0; i < size * size; i++) 
 	{
-		mat[i] = (float)(rand() % 10);
+		double f = (double)rand() / RAND_MAX;
+		mat[i] = (float)(f * 5);
 	}
 }
 
@@ -282,6 +291,86 @@ void SetMatrixAsSequential(float *mat, int size)
 	}
 }
 
+float MatrixResidual(float *cublas, float *test, int w, int h)
+{
+	float dif = 0;
+	for (int i = 0; i < w * h; i++)
+	{
+		// Since cublas matrix is tranposed, swap rows and columns when calculating residual
+		int x = i % w;
+		int y = i / w;
+		int delta = cublas[(x * w) + y] - test[i];
+		dif += delta;
+	}
+
+	return dif;
+}
+
+void TestSize(cublasHandle_t &handle, int size, bool writeOutput)
+{
+	float *testA = new float[size * size];
+	float *testB = new float[size * size];
+
+	FILE *naive_fp; // Naive method file data
+	FILE *opt_fp; // Optimized method file data
+
+	if (writeOutput)
+	{
+		naive_fp = fopen("naive.csv", "a");
+		opt_fp = fopen("opt.csv", "a");
+	}
+
+	// Randomize matrices
+	RandomizeMatrix(testA, size);
+	RandomizeMatrix(testB, size);
+	printf("Testing size of %i x %i...\n", size, size);
+
+	// Run cublas
+	float totalTime0, kernelTime0;
+	float* result0 = CublasMM(handle, testA, testB, size, &kernelTime0, &totalTime0);
+	printf("Cublas MM | Total (ms) = %.3f | Kernel (ms) = %.3f\n", totalTime0, kernelTime0);
+
+	// Run Naive MM
+	float totalTime1, kernelTime1;
+	float* result1 = NaiveMM(testA, testB, size, &kernelTime1, &totalTime1);
+	float residual1 = MatrixResidual(result0, result1, size, size);
+	printf("Naive MM | Total (ms) = %.3f | Kernel (ms) = %.3f | Cublas Residual = %.6f\n", totalTime1, kernelTime1, residual1);
+	if (writeOutput && naive_fp)
+	{
+		fprintf(naive_fp, "%i, %.3f, %.3f\n", size, totalTime1, kernelTime1);
+	}
+
+	// Run Optimized MM
+	float totalTime2, kernelTime2;
+	float* result2 = OptimizedMM(testA, testB, size, &kernelTime2, &totalTime2);
+	float residual2 = MatrixResidual(result0, result2, size, size);
+	printf("Optimized MM | Total (ms) = %.3f | Kernel (ms) = %.3f | Cublas Residual = %.6f\n", totalTime2, kernelTime2, residual2);
+	if (writeOutput && opt_fp)
+	{
+		fprintf(opt_fp, "%i, %.3f, %.3f\n", size, totalTime1, kernelTime1);
+	}
+
+	// Delete input matrices
+	delete[] testA;
+	delete[] testB;
+
+	// Delete results
+	delete[] result0;
+	delete[] result1;
+	delete[] result2;
+
+	// Close file pointers
+	if (writeOutput && naive_fp)
+	{
+		fclose(naive_fp);
+	}
+
+	if (writeOutput && opt_fp)
+	{
+		fclose(opt_fp);
+	}
+}
+
 int main()
 {
 	srand(0);
@@ -290,25 +379,11 @@ int main()
 	cublasHandle_t handle;
 	cublasCreate(&handle);
 
-	const int size = 4;
-
-	float *testA = new float[size * size];
-	float *testB = new float[size * size];
-
-	SetMatrixAsSequential(testA, size);
-	SetMatrixAsSequential(testB, size);
-
-	//float elapsed = 0;
-	//float* result1 = NativeMM(testA, testB, size, false, &elapsed);
-	//printf("Elapsed = %.6fms\n", elapsed);
-
-	float* result2 = CublasMM(handle, testA, testB, size);
-
-	PrintMatrix(result2, size, size);
-
-	// Delete results
-	delete[] result1;
-	delete[] result2;
+	int sizes[8]{ 64, 128, 256, 512, 1024, 2048, 4096, 8192 };
+	for (int i = 0; i < 8; i++)
+	{
+		TestSize(handle, sizes[i], true);
+	}
 
 	// Destroy the cublas handle
 	cublasDestroy(handle);
